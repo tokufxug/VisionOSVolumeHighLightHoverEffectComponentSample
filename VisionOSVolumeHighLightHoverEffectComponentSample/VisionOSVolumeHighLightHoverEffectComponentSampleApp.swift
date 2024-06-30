7cfde3bfd393c6baad94a6a3f7120cd17141901f
tokufxug <tokufxug@gmail.com>
2024-06-30 22:12:50 +0900

    Initial Commit

---
+++ VisionOSVolumeHighLightHoverEffectComponentSample/VisionOSVolumeHighLightHoverEffectComponentSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionOSVolumeHighLightHoverEffectComponentSampleApp.swift
+//  VisionOSVolumeHighLightHoverEffectComponentSample
+//
+//  Created by Sadao Tokuyama on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSVolumeHighLightHoverEffectComponentSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
---
+++ VisionOSVolumeHighLightHoverEffectComponentSampleTests/VisionOSVolumeHighLightHoverEffectComponentSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSVolumeHighLightHoverEffectComponentSampleTests.swift
+//  VisionOSVolumeHighLightHoverEffectComponentSampleTests
+//
+//  Created by Sadao Tokuyama on 6/30/24.
+//
+
+import Testing
+@testable import VisionOSVolumeHighLightHoverEffectComponentSample
+
+struct VisionOSVolumeHighLightHoverEffectComponentSampleTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
